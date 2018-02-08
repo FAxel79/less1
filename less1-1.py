4ebfcf2893d3c8a8af6989b21addd8f6ec8e97e5
FAxel79 <faxel79@gmail.com>
2018-02-08 12:00:56 +0300

    gkk

--- less1-1.py
+++ less1-1.py
@@ -1,6 +1,15 @@
+import pymystem3
+from  pymystem3 import Mystem
+
+tt = 'Простая проверочка лемматизации русского текста на вскидку'
+m = Mystem()
+lem = m.lemmatize(tt)
+
+
 print('Старт')
 rr='Самый крутой тест'
 
 print(rr)
+print( lem)
 
 
